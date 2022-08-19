192de0f971d2b083aeca1bf3ee5cfb35158709ce
andersonprovox <andersongfonseca@gmail.com>
2022-08-19 16:27:06 -0300

    adicionando a funcionalidade de escolher qual tarefa excluir e alterar

---
+++ README.md
@@ -0,0 +1,6 @@
+# Projeto de lista de tarefas
+Criado o projeto de uma aplicação de lista de tarefas com HTML, CSS e JavaScript
+
+Aqui é possível adicionar, alterar e excluir tarefas.
+
+
--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
+let oldInputValue;
 //funcoes
 const saveTodo = (text) => {
     const todo = document.createElement("div");
@@ -36,6 +37,24 @@ const saveTodo = (text) => {
     todoInput.focus();
 };
 
+const toggleForms = () => {
+    editForm.classList.toggle("hide");
+    todoForm.classList.toggle("hide");
+    todoList.classList.toggle("hide");
+};
+
+const updateTodo = (text) => {
+    const todos = document.querySelectorAll(".todo")
+
+    todos.forEach((todo) => {
+        let todotile = todo.querySelector("h3")
+
+        if(todoTitle.innerText === oldInputValue){
+            totoTitle.innerText = text;
+        }
+    })
+}
+
 //Eventos
 todoForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -50,6 +69,11 @@ todoForm.addEventListener("submit", (e) => {
 document.addEventListener("click", (e) => {
     const targetEl = e.target;
     const parentEl = targetEl.closest("div");
+    let todoTitle;
+
+    if(parentEl && parentEl.querySelector("h3")){
+        todoTitle = parentEl.querySelector("h3").innerText;
+    }
 
     if(targetEl.classList.contains("finish-todo")){
         parentEl.classList.toggle("done");
@@ -58,4 +82,30 @@ document.addEventListener("click", (e) => {
     if(targetEl.classList.contains("remove-todo")){
         parentEl.remove();
     }
+
+    if(targetEl.classList.contains("edit-todo")){
+        toggleForms();
+
+        editInput.value = todotitle
+        oldInputValue = todoTitle
+    }
 });
+
+cancelEditBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    toggleForms();
+});
+
+editForm.addEventListener("submit", (e) => {
+    
+    e.preventDefault()
+
+    const editInputValue = editInput.value
+
+    if(editInputValue) {
+        updateTodo(editInputValue)
+    }
+
+    toggleForms();
+})
